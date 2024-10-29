542b379dcf43e3eda02e738dea47dd9dce09a5d6
RJLarot <1801101934@student.buksu.edu.ph>
2024-10-29 08:40:05 +0800

    Duplication

--- Server/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {getStudent, getStudents} from "../controller/studentController.js"
-
-const router = express.Router();
-
-router.get("/", getStudent)
-router.get("/:id", getStudents)
-
-export default router
